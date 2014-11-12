f9336e800acf92421c373d04d9f82497289037f3
Cynthia O'Donnell <mimibambino@gmail.com>
2014-11-12 13:45:16 +0100

    remove all timing API calls

--- views/js/main.js
+++ views/js/main.js
@@ -414,14 +414,14 @@ for (var i = 2; i < 100; i++) {
 
 //////////////////////////// Cynthia's Magic Pizza Resizer ///////////////////////////
 
-//var resizePizzas = function(){}
+//var resizePizzas = function(size){}
 
 
 /////////////////////////  End of Cynthia's Magic Pizza Resizer //////////////////////
 
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
-var resizePizzas = function(size) { 
-  //window.performance.mark("mark_start_resize");   // User Timing API function
+// pizza.html sends a value of 1, 2, or 3 as the size parameter
+var resizePizzas = function(size) {
 
 //////////  Save document query to variable  ////////////
   function changeSliderLabel(size){
@@ -477,61 +477,27 @@ var resizePizzas = function(size) {
   }
 
   changePizzaSizes(size);
-
-  // User Timing API is awesome
-  //window.performance.mark("mark_end_resize");
-  //window.performance.measure("measure_pizza_resize", "mark_start_resize", "mark_end_resize");
-  var timeToResize = window.performance.getEntriesByName("measure_pizza_resize");
-  /*console.log("Time to resize pizzas: " + timeToResize[0].duration + "ms");*/
 }
-
-//window.performance.mark("mark_start_generating"); // collect timing data
-
-// User Timing API again. These measurements tell you how long it took to generate the initial pizzas
-//window.performance.mark("mark_end_generating");
-//window.performance.measure("measure_pizza_generation", "mark_start_generating", "mark_end_generating");
-//var timeToGenerate = window.performance.getEntriesByName("measure_pizza_generation");
-/*console.log("Time to generate pizzas on load: " + timeToGenerate[0].duration + "ms");*/
 
 // Iterator for number of times the pizzas in the background have scrolled.
 // Used by updatePositions() to decide when to log the average time per frame
 var frame = 0;
 
-// Logs the average amount of time per 10 frames needed to move the sliding background pizzas on scroll.
-/*function logAverageFrame(times) {   // times is the array of User Timing measurements from updatePositions()
-  var numberOfEntries = times.length;
-  var sum = 0;
-  for (var i = numberOfEntries - 1; i > numberOfEntries - 11; i--) {
-    sum = sum + times[i].duration;
-  }
-  /*console.log("Average time to generate last 10 frames: " + sum / 10 + "ms");*/
-//}*/
-
 // The following code for sliding background pizzas was pulled from Ilya's demo found at:
 
-// Figure out why he is able to get under 60fps!!! 
+/////////// Figure out why he is able to get under 60fps!!! ////////////
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
 
 function updatePositions() {
   frame++;
-  //window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-
-  // User Timing API to the rescue again. Seriously, it's worth learning.
-  // Super easy to create custom metrics.
-  //window.performance.mark("mark_end_frame");
-  //window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
-  //if (frame % 10 === 0) {
-    //var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
-    /*logAverageFrame(timesToUpdatePosition);*/
-  //}
 }
 
 // runs updatePositions on scroll
